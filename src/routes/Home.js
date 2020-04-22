4e6b57b7fa64c5e78b1c4364602811d83de97bee
= <=>
2020-04-22 17:04:23 -0500

    added homepage

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,14 +4,68 @@ export default class Home extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-
+            workouts: []
         }
+    }
+
+    handleApiWorkoutResponse = (response) => {
+        if (response.success) {
+            let workouts = [];
+            response.workouts.map(workout => {
+                // objects are implemented as hash tables anyway for constant lookup time
+                let hash = {};
+                workout.sets.map(set => {
+                    if (hash[set] === true) {}
+                    else {
+                        hash[set] = true;
+                    }
+                })
+
+                // getting all propreties in hash table
+                let sets = Object.getOwnPropertyNames(hash);
+                workout.sets = sets;
+
+                workouts.push(workout)
+            })
+            this.setState({
+                workouts: workouts,
+            })
+        }
+    }
+
+    getNewestWorkouts = () => {
+        fetch('http://localhost:8000/get/all/workouts/')
+        .then(response => response.json())
+        .then(response => this.handleApiWorkoutResponse(response))
+        .catch(err => {})
+    }
+
+    componentWillMount() {
+        this.getNewestWorkouts()
     }
 
     render() {
         return (
             <div>
-                home
+                <div className="display-4" style={{
+                    margin: .5+'em'
+                }}>Latest 20 workouts</div>
+                <div className="row" style={{
+                    margin: 1+'em',
+                }}>
+                    
+                    {this.state.workouts.map(workout => (
+                        <div className='col card' style={{
+                            maxWidth: 27+'em',
+                            minWidth: 20+'em',
+                            margin: 1+'em'
+                        }}>
+                            <span className="lead" style={{
+                                margin: 1+'em'
+                            }}>{workout.sets.join(', ')}</span>
+                        </div>
+                    ))}
+                </div>
             </div>
         )
     }
